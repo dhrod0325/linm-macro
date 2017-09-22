1a67e1f7eb10bf80c584d2f6ac653b509c695d89
dhrod5457 <dhrod5457@gmail.com>
2017-09-23 01:40:39 +0900

    커밋커밋

--- module-projects/macro-core/src/main/java/lm/macro/auto/common/LmCommon.java
+++ module-projects/macro-core/src/main/java/lm/macro/auto/common/LmCommon.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LmCommon {
-    public static final String VERSION = "0.81";
+    public static final String VERSION = "0.82";
 
     private static final String DIR_PATH = getDirPath();
 
